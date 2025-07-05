ceb944aca9017bbaf69f0d90566c081781f80892
DI-daliana <diegoalejdavila@gmail.com>
2025-07-04 23:42:35 -0500

    First commit

--- main.py
+++ main.py
@@ -1,25 +1,58 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.get_init()
     
+    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    AsteroidField.containers =(updatable)
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    asteroid = AsteroidField()
+    player = Player(SCREEN_WIDTH/2 , SCREEN_HEIGHT/2)
+    
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     game = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
     while game==True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-            
-        screen.fill((0,0,0))    
+        clock.tick(60)
+        dt = clock.tick(60)/1000
+        
+        screen.fill((0,0,0))
+        for p in drawable:
+            p.draw(screen)
+
+        for p in updatable:
+            p.update(dt)
+
+        for ast in asteroids:
+            if ast.collisions(player) == True:
+                sys.exit("Game Over") 
+              
         pygame.display.flip()
+        
     
 
 if __name__ == "__main__":
